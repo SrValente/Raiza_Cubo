6be5391b4b70488731a7c8608db189e4cb5912ea
SrValente <143466264+SrValente@users.noreply.github.com>
2025-03-20 16:32:09 -0300

    Update 0_👤_Central_Aluno.py

--- pages/0_👤_Central_Aluno.py
+++ pages/0_👤_Central_Aluno.py
@@ -1,12 +1,3 @@
 import streamlit as st
-import pandas as pd
-import requests
-from requests.auth import HTTPBasicAuth
-import urllib3
-import pyodbc
-import io
-from datetime import datetime
-import xml.etree.ElementTree as ET
-from xml.sax.saxutils import escape
 
 st.title("EM BREVE")
